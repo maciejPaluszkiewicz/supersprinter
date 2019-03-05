99ec494a28e13c8282c537f718919f9f7953672c
maciejPaluszkiewicz <maciej.j.paluszkiewicz@gmail.com>
2019-03-05 21:26:16 +0100

    add json, user stories

---
+++ data/userStories.js
@@ -0,0 +1,31 @@
+{
+  "stories": [
+    {
+      "id": 1078696530898,
+      "story_title": "List all User Stories",
+      "user_story": "As a User, I want to see all the previously saved User Stories, So that I get an overview of all requirements",
+      "acceptance_criteria": "Given that there are any number ({0..n}) of saved User Stories, When I open the website (/), Then ensure I see a table with all the stored data",
+      "business_value": "1000",
+      "estimation": "4",
+      "status": "todo"
+    },
+    {
+      "id": 1121297730998,
+      "story_title": "Add new User Stories",
+      "user_story": "As a User, I want to be able to add a new User Stories, So that I can extend the list of requirements",
+      "acceptance_criteria": "When I open the website (/), And I click the \"Add User Story\" link Then ensure I see a form where I can add details of the new User Story, And if I submit the form, this User Story gets saved.",
+      "business_value": "500",
+      "estimation": "2",
+      "status": "todo"
+    },
+    {
+      "id": 1133692330186,
+      "story_title": "Update Existing User Stories",
+      "user_story": "As a User, I want to be able to change details of an existing User Story, So that I can keep them up-to-date with the current info.",
+      "acceptance_criteria": "When I open the website (/), And I click the title of a User Story, Then ensure I see a form prefilled with the details of the User Story, And when I submit the changed data, ensure it gets save, And ensure it overwrites the previous details, not creating a new User Story.",
+      "business_value": "200",
+      "estimation": "2",
+      "status": "todo"
+    }
+  ]
+}
